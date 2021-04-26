7dc701cf6d20c7d12594754f4485b47ec01041cf
aliashraf94 <adaydreamer10@gmail.com>
2021-04-26 20:59:45 +0200

    Lisa Simpson part1

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,31 @@
 // Write code here to communicate with Github
+URL = "https://api.github.com/users/aliashraf94/repos";
+
+let getRepos = () => {
+  fetch(URL)
+    .then(response => response.json())
+    .then(reposList => renderRepos(reposList))
+    .catch(error => console.log(error));
+};
+
+let renderRepos = reposList => {
+  reposList.forEach(repo => {
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+    let ul = document.querySelector("#repos-list");
+    li.appendChild(link);
+    ul.appendChild(li);
+    li.innerText = repo.full_name;
+    link.setAttribute("href", repo.html_url);
+    // let x = document.querySelector('#repos-count')
+    // x.innerText = reposList.length
+    countRepos(reposList);
+  });
+};
+
+let countRepos = gitRepos => {
+  let x = document.querySelector("#repos-count");
+  x.innerText = gitRepos.length;
+};
+
+getRepos();
